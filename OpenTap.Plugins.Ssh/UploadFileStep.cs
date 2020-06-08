4c1600cb0e0af722a7787245a3a2d340724a432b
Lars Nielsen <lars.mikkelsen@keysight.com>
2020-06-08 11:28:49 +0000

    Resolve "Scp upload file gives error "Unexpected filename""
    - fixed upload file issue by downgrading ssh.net package one version
    - fixed issue with wrong checksum of uploaded file
    - stream file directly from disk when uploading

--- OpenTap.Plugins.Ssh/UploadFileStep.cs
+++ OpenTap.Plugins.Ssh/UploadFileStep.cs
@@ -24,51 +24,13 @@ namespace OpenTap.Plugins.Ssh
     [Display("Upload File", Group: "SSH", Description: "Uses SCP to upload a file to a remote host defined by an SSH Session step, SSH Instrument or SSH Dut.")]
     public class ScpUploadFileStep : SshStepBase
     {
-        private string fileContentAsBase64 = null;
-
         #region Settings
         [Display(Name: "Remote Upload Path", Group: "Action")]
         public string UploadPath { get; set; }
 
         [FilePath()]
         [Display(Name: "Local File To Upload", Group: "Action")]
-        public string FileBase64
-        {
-            get
-            {
-                return fileContentAsBase64;
-            }
-            set
-            {
-                bool fallback = true;
-                FileInfo fi = null;
-                try
-                {
-                    fi = new FileInfo(value);
-                    if (fi.Exists)
-                    {
-                        string fileContent = File.ReadAllText(value);
-                        fileContentAsBase64 = Base64Encoding.EncodeBase64(fileContent);
-                        fallback = false;
-                    }
-                }
-                catch
-                {
-                }
-
-                if (fallback)
-                {
-                    //if (Base64Encoding.IsBase64(value))
-                    //{
-                    fileContentAsBase64 = value;
-                    //}
-                    //else
-                    //{
-                    //    hotContentAsBase64 = null;
-                    //}
-                }
-            }
-        }
+        public string FileToUpload { get; set; }
         #endregion
 
         /// <summary>
@@ -84,8 +46,10 @@ namespace OpenTap.Plugins.Ssh
         /// </summary>
         public override void Run()
         {
-            string fileContent = Base64Encoding.DecodeBase64(fileContentAsBase64);
-            SshResource.ScpClient.Upload(new MemoryStream(Encoding.ASCII.GetBytes(fileContent)), UploadPath);
+            using(FileStream fs = File.OpenRead(FileToUpload))
+            {
+                SshResource.ScpClient.Upload(fs, UploadPath);
+            }
         }
     }
 }
