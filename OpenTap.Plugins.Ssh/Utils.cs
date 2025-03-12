5eaaacdc1736cf4d4ee5af185c1b488becd831a5
Rolf Madsen <rolf_madsen@keysight.com>
2025-03-12 14:39:57 +0100

    Added utils which was missing

---
+++ OpenTap.Plugins.Ssh/Utils.cs
@@ -0,0 +1,14 @@
+﻿namespace OpenTap.Plugins.Ssh
+{
+    public class Utils
+    {
+        public static string FriendlySize(long size)
+        {
+            if (size < 1024)
+                return $"{size} B";
+            if (size < ((1024 * 1024)))
+                return $"{(size/1024.0):F1} kB";
+            return $"{(size/ (1024 * 1024)):F1} MB";
+        }
+    }
+}
